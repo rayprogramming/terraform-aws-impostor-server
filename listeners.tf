9d4ba1d8398643552e1a6e0ec7ab10cff1126d1d
James Ray <rihoj1@gmail.com>
2022-12-27 23:41:31 -0500

    feat(gameserver): add listeners and target group
    feat(matchserver): add listeners and target group

--- listeners.tf
+++ listeners.tf
@@ -1,1 +1,35 @@
-// TODO(rihoj) add lb listeners
+resource "aws_lb_listener" "http_443" {
+  load_balancer_arn = aws_lb.match.arn
+  port = "443"
+  protocol = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+  certificate_arn = "" // TODO(rihoj) make resource
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  }
+}
+
+resource "aws_lb_listener" "http_80" {
+  load_balancer_arn = aws_lb.match.arn
+  port = "80"
+  protocol = "HTTP"
+  default_action {
+    type = "redirect"
+    redirect {
+      port = "433"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "udp_22022" {
+  load_balancer_arn = aws_lb.game.arn
+  port = "22022"
+  protocol = "UDP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.udp.arn
+  }
+}
---
+++ targetgroups.tf
@@ -0,0 +1,24 @@
+resource "aws_lb_target_group" "http" {
+  name = "amongus-http-targetgroup"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = var.instance_subnet.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "http" {
+  target_group_arn = aws_lb_target_group.http.arn
+  target_id = aws_instance.impostor.id
+  port = 22023
+}
+resource "aws_lb_target_group" "udp" {
+  name = "amongus-udp-targetgroup"
+  port = 22022
+  protocol = "HTTP"
+  vpc_id = var.instance_subnet.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "udp" {
+  target_group_arn = aws_lb_target_group.udp.arn
+  target_id = aws_instance.impostor.id
+  port = 22022
+}
