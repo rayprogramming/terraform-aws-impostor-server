0623b216f790abd40e9f399d56ec3a2007ed5d2e
James Ray <rihoj1@gmail.com>
2022-12-27 21:45:27 -0500

    feat(gameserver): add route53 record

--- route53.tf
+++ route53.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "matchmaker" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "gameserver" {
+  zone_id = var.zone_id
+  name = "amongus"
+  type = "A"
+
+  alias {
+    name = aws_lb.game.dns_name
+    zone_id = aws_lb.game.zone_id
+    evaluate_target_health = true
+  }
+}
