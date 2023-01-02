4c2f233e149036367ae7392daa88f62befa75b64
James Ray <rihoj1@gmail.com>
2023-01-01 19:48:15 -0500

    fix(matchmaker): add acm arn

--- listeners.tf
+++ listeners.tf
@@ -3,7 +3,7 @@ resource "aws_lb_listener" "http_443" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "" // TODO(rihoj) make resource
+  certificate_arn   = module.acm.acm_certificate_arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.http.arn
